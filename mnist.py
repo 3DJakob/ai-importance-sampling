8b6dab35edc0f7d78565b2c282a65a30848f34e6
Jakob Unnebäck <jakob@folkdatorn.se>
2023-02-08 11:44:27 +0100

    Refactoring

--- mnist.py
+++ mnist.py
@@ -1,6 +1,9 @@
 import torch
 import torchvision
 from helper import plot
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 n_epochs = 10
 batch_size_train = 64
@@ -34,22 +37,6 @@ test_loader = torch.utils.data.DataLoader(
 examples = enumerate(test_loader)
 batch_idx, (example_data, example_targets) = next(examples)
 
-import matplotlib.pyplot as plt
-
-fig = plt.figure()
-for i in range(6):
-  plt.subplot(2,3,i+1)
-  plt.tight_layout()
-  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
-  plt.title("Ground Truth: {}".format(example_targets[i]))
-  plt.xticks([])
-  plt.yticks([])
-fig
-
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -68,6 +55,46 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x)
 
+    def trainEpoch(self, epoch):
+      network.train()
+      for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = network(data)
+        loss = F.cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+          acc = self.test()
+          accPlot.append(acc)
+          plot(accPlot, None)
+
+          print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, batch_idx * len(data), len(train_loader.dataset),
+            100. * batch_idx / len(train_loader), loss.item()))
+          train_losses.append(loss.item())
+          train_counter.append(
+            (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+          # torch.save(network.state_dict(), '/results/model.pth')
+          # torch.save(optimizer.state_dict(), '/results/optimizer.pth')
+
+    def test(self):
+      network.eval()
+      test_loss = 0
+      correct = 0
+      with torch.no_grad():
+        for data, target in test_loader:
+          output = network(data)
+          test_loss += F.nll_loss(output, target, size_average=False).item()
+          pred = output.data.max(1, keepdim=True)[1]
+          correct += pred.eq(target.data.view_as(pred)).sum()
+      test_loss /= len(test_loader.dataset)
+      test_losses.append(test_loss)
+      print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+      accTensor = correct / len(test_loader.dataset)
+      return accTensor.item()
+
 network = Net()
 # optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
 optimizer = optim.Adam(network.parameters(), lr=learning_rate)
@@ -79,50 +106,7 @@ test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
 
 accPlot = []
 
-def train(epoch):
-  network.train()
-  for batch_idx, (data, target) in enumerate(train_loader):
-    optimizer.zero_grad()
-    output = network(data)
-    loss = F.cross_entropy(output, target)
-    loss.backward()
-    optimizer.step()
-    if batch_idx % log_interval == 0:
-      acc = test()
-      accPlot.append(acc)
-      plot(accPlot, None)
-
-      print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-        epoch, batch_idx * len(data), len(train_loader.dataset),
-        100. * batch_idx / len(train_loader), loss.item()))
-      train_losses.append(loss.item())
-      train_counter.append(
-        (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
-      # torch.save(network.state_dict(), '/results/model.pth')
-      # torch.save(optimizer.state_dict(), '/results/optimizer.pth')
-
-def test():
-  network.eval()
-  test_loss = 0
-  correct = 0
-  with torch.no_grad():
-    for data, target in test_loader:
-      output = network(data)
-      test_loss += F.nll_loss(output, target, size_average=False).item()
-      pred = output.data.max(1, keepdim=True)[1]
-      correct += pred.eq(target.data.view_as(pred)).sum()
-  test_loss /= len(test_loader.dataset)
-  test_losses.append(test_loss)
-  print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-    test_loss, correct, len(test_loader.dataset),
-    100. * correct / len(test_loader.dataset)))
-  accTensor = correct / len(test_loader.dataset)
-  return accTensor.item()
-
-
-test()
-
 for epoch in range(1, n_epochs + 1):
-  train(epoch)
+  network.trainEpoch(epoch)
 
 
