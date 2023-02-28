f84b12536c2b75691f743e3af0e0d51f30fdebda
Jakob Unnebäck <jakob@folkdatorn.se>
2023-02-28 11:58:51 +0100

    equalLossTrainLoader

--- importance_samplers/loss_sort.py
+++ importance_samplers/loss_sort.py
@@ -2,6 +2,7 @@ import torch
 from libs.samplers import OrderedSampler
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+from math import floor
 
 
 def lossSortTrainLoader(
@@ -40,3 +41,54 @@ def getLossGraph(
       plt.pause(7)
     
     return losses
+
+def equalLossTrainLoader(
+  train_loader,
+  network,
+  batch_size_train: int
+  ):
+  with torch.no_grad():
+    losses = torch.zeros(len(train_loader.dataset))
+    for batch_idx, (data, target) in enumerate(train_loader):
+      output = network(data)
+      loss = F.cross_entropy(output, target, reduction='none')
+      losses[batch_idx * len(data):batch_idx * len(data) + len(data)] = loss
+
+    sortedLosses, sortedIndices = torch.sort(losses, descending=False)
+    finalOrder = torch.zeros(len(sortedIndices), dtype=torch.long)
+
+    # sample evenly from sortedIndices so each batch has a range of losses
+    STEP_SIZE = floor(len(sortedIndices) / batch_size_train)
+    for i in range(0, len(sortedIndices)):
+      batchIndex = i % batch_size_train # 0-99
+      batchesCompleted = floor(i / batch_size_train) # 0-599
+      # finalOrder[i] = sortedIndices[batchIndex * batch_size_train + batchesCompleted]
+      finalOrder[i] = sortedIndices[batchIndex * STEP_SIZE + batchesCompleted]
+    # arrange losses in indice order defined by finalOrder
+    lossSordedByFinalOrder = torch.zeros(len(sortedIndices))
+    for i in range(0, len(sortedIndices)):
+      lossSordedByFinalOrder[i] = losses[int(finalOrder[i])]
+
+    # foo, fooindex = torch.sort(finalOrder, descending=True)
+
+    # test = foo.detach().numpy()
+
+    # # testHasDuplicates = False
+    # # for i in range(0, len(test) - 1):
+    # #   if (test[i] == test[i + 1]):
+    # #     testHasDuplicates = True
+    
+    # # if (testHasDuplicates):
+    # #   print("test has duplicates")
+
+    # firstThousands = test[0:1000]
+    # plt.plot(test)
+    # plt.show()
+    # plt.pause(7)
+
+
+    train_loader2 = torch.utils.data.DataLoader(train_loader.dataset, batch_size=batch_size_train, sampler=OrderedSampler(finalOrder), shuffle=False)
+
+    # train_loader2 = torch.utils.data.DataLoader(train_loader.dataset, batch_size=batch_size_train, sampler=OrderedSampler(sortedIndices), shuffle=False)
+
+  return train_loader2
